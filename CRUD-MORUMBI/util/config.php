355fc130fdd164af24f77fba2e70d35eabace523
taegvils <101215542+taegvils@users.noreply.github.com>
2023-09-29 15:01:43 -0300

    Update config.php

--- CRUD-MORUMBI/util/config.php
+++ CRUD-MORUMBI/util/config.php
@@ -1,10 +1,10 @@
 <?php
 
 //Configurar essas variáveis de acordo com o seu ambiente
-define("DB_HOST", "mysql-server");
+define("DB_HOST", "localhost");
 define("DB_NAME", "visitamorumbi");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
 
 //Constante com a URL do sistema
-define("BASE_URL", "/htdocs/crud_morumbi");
+define("BASE_URL", "/crud_morumbi");
